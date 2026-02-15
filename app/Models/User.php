7d4f87ed95a4ddd607f54c887d1a3ed0666ea3c6
Oleg Mamaev <20475296+debug4096@users.noreply.github.com>
2026-02-15 14:50:40 +0300

    feat: add medication relationship to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(DailyEntry::class);
     }
+
+    public function medications(): HasMany
+    {
+        return $this->hasMany(Medication::class);
+    }
 }
