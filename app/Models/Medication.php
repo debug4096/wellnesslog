9ac79703f07c3860421b049cce773ecbfa542a86
Oleg Mamaev <20475296+debug4096@users.noreply.github.com>
2026-02-17 21:43:51 +0300

    feat: add name mutator and formattedDosage accessor to Medication

--- app/Models/Medication.php
+++ app/Models/Medication.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Enums\MedicationFrequency;
 use App\Enums\MedicationUnit;
 use Database\Factories\MedicationFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -22,6 +23,20 @@ class Medication extends Model
         'unit',
         'frequency',
     ];
+
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            set: fn(string $value) => mb_strtolower($value)
+        );
+    }
+
+    protected function formattedDosage(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => $this->dosage . ' ' . $this->unit->value
+        );
+    }
 
     public function user(): BelongsTo
     {
