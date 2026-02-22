de408f88f5b50f245e2464956543de6e1ab382ba
Oleg Mamaev <20475296+debug4096@users.noreply.github.com>
2026-02-22 12:15:58 +0300

    feat: add Sanctum authentication with register, login, logout, me endpoints

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'     => 'required|string|min:3|max:255',
+            'email'    => 'required|string|email|unique:users,email|max:255',
+            'password' => 'required|string|confirmed|min:8|max:128',
+        ];
+    }
+}
